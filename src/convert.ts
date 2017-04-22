8065ba626c8b0c6eb2417d2dc7042331b83ef26e
Sheyne Anderson <sheyne@sheyne.com>
2017-04-22 00:19:22 -0600

    improve convert tool

--- src/convert.ts
+++ src/convert.ts
@@ -80,6 +80,8 @@ const stringType = new Type.Primitive("string");
 const numberType = new Type.Primitive("number");
 const booleanType = new Type.Primitive("boolean");
 const pointType = new Type.Record(new Map([["x", numberType], ["y", numberType]]));
+const colorType = new Type.Primitive("color");
+const widthType = new Type.Union([new Type.Literal("thin"), new Type.Literal("medium"), new Type.Literal("thick"), numberType]);
 
 const convert: { [a: string]: undefined | ((plugin: Plugin, sourceFile: string, destFile: string) => Promise<void>) } = {
     ["turing-machine"]: (plugin: Plugin, sourceFile: string, destFile: string) => {
@@ -111,7 +113,18 @@ const convert: { [a: string]: undefined | ((plugin: Plugin, sourceFile: string, 
             coreNode.set("isAcceptState", new Value.Primitive(booleanType, coreNode.environment, accept));
         }, (jEdge, coreEdge) => {
             const read = jEdge.read[0];
-            coreEdge.set("label", new Value.Primitive(stringType, coreEdge.environment, read));
+            coreEdge.set("symbol", new Value.Primitive(stringType, coreEdge.environment, read));
+        });
+    },
+    ["nfa"]: (plugin: Plugin, sourceFile: string, destFile: string) => {
+        return convertInner(plugin, sourceFile, destFile, (jNode, coreNode) => {
+            const accept = Boolean(jNode.final);
+            const start = Boolean(jNode.initial);
+            coreNode.set("isStartState", new Value.Primitive(booleanType, coreNode.environment, start));
+            coreNode.set("isAcceptState", new Value.Primitive(booleanType, coreNode.environment, accept));
+        }, (jEdge, coreEdge) => {
+            const read = jEdge.read[0];
+            coreEdge.set("symbol", new Value.Primitive(stringType, coreEdge.environment, read));
         });
     }
 };
@@ -140,6 +153,7 @@ async function convertInner(plugin: Plugin, sourceFile: string, destFile: string
             const v = model.makeNode();
             v.set("label", new Value.Primitive(stringType, model.environment, label));
             v.set("position", position);
+            v.set("borderColor", new Value.Primitive(colorType, model.environment, "#000000"));
             nodeConvert(n, v);
 
             return [id, v];
@@ -150,6 +164,11 @@ async function convertInner(plugin: Plugin, sourceFile: string, destFile: string
         const from = edge.from[0];
         const to = edge.to[0];
         const e = model.makeEdge(undefined, nodeMap.get(from)!, nodeMap.get(to)!);
+        e.set("color", new Value.Primitive(colorType, model.environment, "#000000"));
+        const widthUnion = new Value.Union(widthType, model.environment);
+        widthUnion.value = new Value.Primitive(numberType, model.environment, 2);
+        e.set("lineWidth", widthUnion);
+        e.set("showDestinationArrow", new Value.Primitive(booleanType, model.environment, true));
         edgeConvert(edge, e);
     }
 
